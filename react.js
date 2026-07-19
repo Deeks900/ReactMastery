027a0c2d67e77d743a0dd0b0ee9c76f0dc072857
Deeksha Sharma <deekshasharma2022@gmail.com>
2026-07-20 00:22:05 +0530

    Episode1Finsihed

--- react.js
+++ react.js
@@ -1,3 +1,37 @@
-heading = React.createElement('h1', {id: 'head'}, "Let's Revise React");
+// heading = React.createElement('h1', {id: 'head'}, "Let's Revise React");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+//Create Nested Structure with Siblings as well
+// const parentDiv = React.createElement('div', {id:'parent'}, 
+//     React.createElement('div', {id:'child'},
+//        [
+//         React.createElement('h1', {id:'head'}, "Let's Revise React"),
+//         React.createElement('h1', {id:'head2'}, "Let's Revise React with Sibling")
+//        ] 
+//     )
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parentDiv);
+
+// //parentDiv is an object which will be converted into HTML by ReactDOM.
+// console.log(parentDiv);
+
+//Create more complex structure 
+
+const headDiv = React.createElement('div', {id:'headDiv'},
+    [
+        React.createElement('div', {id:'child1'}, [
+            React.createElement('h1', {id:'head1'}, "Let's Revise React"),
+            React.createElement('h1', {id:'head2'}, "Let's Revise React with Sibling")
+        ]),
+        React.createElement('div', {id:'child2'}, [
+            React.createElement('h1', {id:'head3'}, "Let's Revise React with Another Sibling"),
+            React.createElement('h1', {id:'head4'}, "Let's Revise React with Yet Another Sibling")
+        ])
+    ]
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(headDiv);
