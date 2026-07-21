b091fbf4062d54082ae7e599ab4b375d77e5688a
Deeksha Sharma <deeksha@Deekshas-MacBook-Air.local>
2026-07-22 00:27:46 +0530

    Episode2_Completed

--- react.js
+++ react.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 // heading = React.createElement('h1', {id: 'head'}, "Let's Revise React");
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
